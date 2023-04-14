6113a63c1cc7265e5c968732c79d1a0395b52cc4
star <87822123+star08-web@users.noreply.github.com>
2023-04-14 22:24:10 +0200

    changed fonts and added a little easter egg

--- js/main.js
+++ js/main.js
@@ -1,39 +1,57 @@
 let bar = document.querySelector(".mobbar");
 let toggler = document.querySelector(".mobmenu");
-let moblink = document.querySelector(".moblink")
-let downloads = document.querySelector(".downnw")
-let dev = document.querySelector(".devnw")
-let music = document.querySelector(".musicnw")
-let downloadsdsk = document.querySelector(".downloads")
-let devdsk = document.querySelector(".devices")
-let musicdsk = document.querySelector(".music")
-let homedsk = document.querySelector(".home")
-let homepage = document.querySelector(".homepage")
-let devpage = document.querySelector(".devicespage")
-let ytbtn = document.querySelector(".youtube-btn")
-let ytchn = document.querySelector(".yt-channel-page-embed")
-let ytclosebtn = document.querySelector(".closebtn-yt")
+let moblink = document.querySelector(".moblink");
+let downloads = document.querySelector(".downnw");
+let dev = document.querySelector(".devnw");
+let music = document.querySelector(".musicnw");
+let downloadsdsk = document.querySelector(".downloads");
+let devdsk = document.querySelector(".devices");
+let musicdsk = document.querySelector(".music");
+let homedsk = document.querySelector(".home");
+let homepage = document.querySelector(".homepage");
+let devpage = document.querySelector(".devicespage");
+let ytbtn = document.querySelector(".youtube-btn");
+let ytchn = document.querySelector(".yt-channel-page-embed");
+let ytclosebtn = document.querySelector(".closebtn-yt");
+let cntclosebtn = document.querySelector(".closebtn-cnt");
+let contactbtn = document.querySelector(".contact-btn");
+let contact = document.querySelector(".contact-form");
+
 toggler.addEventListener("click", function () {
   bar.classList.toggle("open");
-  moblink.classList.toggle("show")
-  downloads.classList.toggle("show")
-  dev.classList.toggle("show")
-  music.classList.toggle("show")
+  moblink.classList.toggle("show");
+  downloads.classList.toggle("show");
+  dev.classList.toggle("show");
+  music.classList.toggle("show");
 });
+
 homedsk.addEventListener("click",function () {
-  homepage.classList.remove("hide")
-  devpage.classList.remove("show")
-})
+  homepage.classList.remove("hide");
+  devpage.classList.remove("show");
+});
+
 downloadsdsk.addEventListener("click",function () {
-  homepage.classList.add("hide")
-})
+  homepage.classList.add("hide");
+  devpage.classList.remove("show");
+});
+
 devdsk.addEventListener("click",function () {
-  homepage.classList.add("hide")
-  devpage.classList.add("show")
-})
+  homepage.classList.add("hide");
+  devpage.classList.add("show");
+});
+
 ytbtn.addEventListener("click",function () {
-  ytchn.classList.add("popopen")
-})
+  ytchn.classList.add("popopen");
+});
+
 ytclosebtn.addEventListener("click",function () {
-  ytchn.classList.remove("popopen")
-})
+  ytchn.classList.remove("popopen");
+});
+
+contactbtn.addEventListener("click",function () {
+  contact.classList.add("popopen");
+});
+
+cntclosebtn.addEventListener("click",function () {
+  contact.classList.remove("popopen");
+});
